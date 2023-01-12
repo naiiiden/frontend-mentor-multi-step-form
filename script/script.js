80bb8f8b2548f209215c67c093ff09684d1f0e3c
naiiiden <na4ko359@gmail.com>
2023-01-12 06:34:49 +0200

    form step 2 label styling when input checked

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-console.log(1);
+// form step 2 input container style when focused
+let step2Inputs = document.querySelectorAll(".form-step-2 input");
+for (let i = 0; i < step2Inputs.length; i++) {
+    step2Inputs[i].addEventListener("change", () => {
+        for (let i = 0; i < step2Inputs.length; i++) {
+            step2Inputs[i].parentNode.style.background = step2Inputs[i].checked ? "hsl(217, 100%, 97%)" : "#fff";
+            step2Inputs[i].parentNode.style.border = step2Inputs[i].checked ? "1px solid hsl(243, 100%, 62%)" : "1px solid hsl(229, 24%, 87%)";
+        };
+    });
+};
