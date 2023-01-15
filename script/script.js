cb22133c87da3f4efba92ba465e0e80d16808d4e
naiiiden <na4ko359@gmail.com>
2023-01-15 17:00:14 +0200

    fs3 label add hover/focus effect

--- script/script.js
+++ script/script.js
@@ -36,7 +36,7 @@ document.getElementById("toggle").addEventListener("click", () => {
 // form step 3 input container style when focused
 let step3Inputs = document.querySelectorAll(".form-step-3 input[type='checkbox']");
 for (let i = 0; i < step3Inputs.length; i++) {
-    step3Inputs[i].addEventListener("change", () => {
+    step3Inputs[i].addEventListener("click", () => {
         for (let i = 0; i < step3Inputs.length; i++) {
             step3Inputs[i].parentNode.style.background = step3Inputs[i].checked ? "hsl(217, 100%, 97%)" : "#fff";
             step3Inputs[i].parentNode.style.border = step3Inputs[i].checked ? "1px solid hsl(243, 100%, 62%)" : "1px solid hsl(229, 24%, 87%)";
